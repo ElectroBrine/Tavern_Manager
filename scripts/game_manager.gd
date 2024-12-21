074ee79b9677f9d91738d5e2101337035e125e67
Darien Piva <darienpiva@gmail.com>
2024-12-20 22:16:07 -0700

    commit

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,17 +2,28 @@ extends Node
 
 var score = 0
 
+# inventory is formatted as {name: {price: float, stock: int}}
 var drink_inventor = {}
 var food_inventor = {}
 
-@onready var daytime: Timer = $Timer
+@onready var daytime: Timer = $GameTime
 
-func _ready() -> void:
-	daytime.start()
-	
 func _process(delta: float) -> void:
 	print(daytime.time_left)
 
 func add_point():
 	score += 1
 	print(score)
+
+func add_drink(key, value = 1):
+	var drink = drink_inventor.get_or_add(key)
+	if drink == null or drink.is_empty():
+		drink = {"price" = 10.0, "stock" = 1}
+		drink_inventor[key] = drink
+	else: drink["stock"] += value
+	
+func get_drinklist():
+	var drink_list = []
+	for drink in drink_inventor.keys():
+		if drink_inventor[drink]["stock"] > 0: drink_list.append(drink)
+	return drink_list
--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,11 +1,29 @@
 extends Node2D
 
 var interactable = true
+var drink_timer = 0
+var tab = 0
+var cooldown = Timer.new()
+var order = ""
 
+@onready var game_manager: Node = %GameManager
+@onready var game_time: Timer = %GameTime
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
 @onready var polygon_2d: Polygon2D = $"Polygon2D"
 @onready var label: Label = $"Label"
+
+func request():
+	cooldown.wait_time = 10
+	cooldown.start()
+	if randf_range(0,1) <= 0.9: return
+	print("ordering drink")
+	var drinks = game_manager.get_drinklist()
+	order = drinks[randi_range(0, len(drinks)-1)]
+	label.text = "one " + order + " please"
+
+func _ready() -> void:
+	cooldown.one_shot = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,6 +31,7 @@ func _process(delta: float) -> void:
 		if Input.is_action_just_pressed("interaction"): 
 			label.visible = true
 			polygon_2d.visible = true
+	if drink_timer == 0 && cooldown.time_left == 0 && len(game_manager.get_drinklist()) > 0 && len(order) == 0: request()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	interactable = true
