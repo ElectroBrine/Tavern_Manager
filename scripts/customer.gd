6f85a1f179b6853200c51868be0ea189ccbe640b
Darien Piva <darienpiva@gmail.com>
2024-12-21 16:55:13 -0700

    Added notification when customers order

--- scripts/customer.gd
+++ scripts/customer.gd
@@ -10,14 +10,15 @@ var order = ""
 @onready var game_time: Timer = %GameTime
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
-@onready var polygon_2d: Polygon2D = $"Polygon2D"
-@onready var label: Label = $"Label"
+@onready var order_box: Node2D = $"Order Box"
+@onready var label: Label = $"Order Box/Label"
+@onready var call_box: Node2D = $"Call box"
 
 func request():
 	cooldown.wait_time = 10
 	cooldown.start()
 	if randf_range(0,1) <= 0.9: return
-	print("ordering drink")
+	call_box.visible = true
 	var drinks = game_manager.get_drinklist()
 	order = drinks[randi_range(0, len(drinks)-1)]
 	label.text = "one " + order + " please"
@@ -29,8 +30,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if interactable == true:
 		if Input.is_action_just_pressed("interaction"): 
-			label.visible = true
-			polygon_2d.visible = true
+			order_box.visible = true
 	if drink_timer == 0 && cooldown.time_left == 0 && len(game_manager.get_drinklist()) > 0 && len(order) == 0: request()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -38,6 +38,5 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	interactable = false
-	if label.visible == true:
-		label.visible = false
-		polygon_2d.visible = false
+	if order_box.visible == true:
+		order_box.visible = false
